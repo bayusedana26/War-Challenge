06e68a99b85cbd74dcc3b8ba93068e018bbda010
Bayu Sedana <bayusedana26@gmail.com>
2022-10-06 15:04:37 +0700

    Initial Commit

---
+++ War-Challenge/War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War-Challenge
+//
+//  Created by Bayu Sedana on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
